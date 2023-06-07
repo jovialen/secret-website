1a3edcef44b14a521d5f80f3e31028c356d54a19
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-06-07 13:51:55 +0200

    feat: add secret and get random secret from collection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,39 @@
 <script>
-  import { collection } from 'firebase/firestore';
+  import { collection, addDoc, getDocs } from 'firebase/firestore';
   import { FirebaseApp } from 'sveltefire';
 
   import { firestore } from '$lib/firebase.js';
+
+  let secretCollection = collection(firestore, "secrets")
+  let secretText = ""
+
+  function addSecret() {
+    addDoc(secretCollection, {
+      text: secretText,
+      complaints: 0,
+    })
+    secretText = ""
+  }
+
+  async function getRandomSecret() {
+    let secretsQuery = await getDocs(secretCollection)
+    let secrets = []
+    secretsQuery.forEach(secret => {
+      secrets.push(secret)
+    })
+    let secret = secrets[Math.floor(Math.random() * secrets.length)].data();
+    console.log(secret)
+  }
 </script>
 
 <FirebaseApp {firestore}>
   <h1>Secrets</h1>
-
   <p>Leave a secret, take a secret!</p>
+
+  <form on:submit={addSecret}>
+    <input type="text" placeholder="Leave your secret here..." bind:value={secretText} required />
+    <button type="submit">Here's my secret, give me one of yours</button>
+  </form>
+
+  <button on:click={getRandomSecret}>Just give me a secret</button>
 </FirebaseApp>
