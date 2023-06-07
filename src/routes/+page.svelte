252ac702f19bd528554f9579bfe6aefc56f0ea7e
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-06-07 16:59:48 +0200

    refactor: remove sveltefire as dependency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
   import { collection, addDoc, getDocs } from 'firebase/firestore';
-  import { FirebaseApp } from 'sveltefire';
 
   import { firestore } from '$lib/firebase.js';
 
   let secretCollection = collection(firestore, "secrets")
   let addSecretText = ""
-  let getSecret = undefined
+  let revealedSecret = undefined
 
   function addSecret() {
     addDoc(secretCollection, {
@@ -22,23 +21,20 @@
     secretsQuery.forEach(secret => {
       secrets.push(secret)
     })
-    getSecret = secrets[Math.floor(Math.random() * secrets.length)].data();
+    revealedSecret = secrets[Math.floor(Math.random() * secrets.length)].data()
   }
 </script>
 
-<FirebaseApp {firestore}>
-  <h1>Secrets</h1>
-  <p>Leave a secret, take a secret!</p>
+<h1>Secrets</h1>
+<p>Leave a secret, take a secret!</p>
 
-  <form on:submit={addSecret}>
-    <input type="text" placeholder="Leave your secret here..." bind:value={addSecretText} required />
-    <button type="submit">Here's my secret, give me one of yours</button>
-  </form>
+<form on:submit={addSecret}>
+  <input type="text" placeholder="Leave your secret here..." bind:value={addSecretText} required />
+  <button type="submit">Here's my secret, give me one of yours</button>
+</form>
 
-  {#if getSecret != undefined}
-    <p>{getSecret.text}</p>
-  {/if}
+{#if revealedSecret != undefined}
+  {revealedSecret.text}
+{/if}
 
-
-  <button on:click={getRandomSecret}>Just give me a secret</button>
-</FirebaseApp>
+<button on:click={getRandomSecret}>Just give me a secret</button>
